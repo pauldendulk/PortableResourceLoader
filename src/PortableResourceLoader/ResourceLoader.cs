14f6c6e3ea6a76139c695f339a089846ec1c6f53
pauldendulk <pauldendulk@gmail.com>
2014-05-12 15:23:27 +0200

    adding ResourceLoader.cs and gps2.png

---
+++ src/PortableResourceLoader/ResourceLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace PortableResourceLoader
+{
+    public class ResourceLoader
+    {
+        public Stream Load(string path, string assembly)
+        {
+            throw new Exception("Add a reference PortableResourceLoader to your main application");
+        }
+    }
+}
