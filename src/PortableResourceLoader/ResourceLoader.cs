61ced466b641a9cd25a1c690bde6573b87f9eab3
pauldendulk <pauldendulk@gmail.com>
2014-05-17 16:24:24 +0200

    Directly loading from the PCL itself

--- src/PortableResourceLoader/ResourceLoader.cs
+++ src/PortableResourceLoader/ResourceLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace PortableResourceLoader
@@ -8,7 +9,37 @@ namespace PortableResourceLoader
     {
         public Stream Load(string path, Assembly assembly)
         {
-            throw new Exception("Add a reference PortableResourceLoader to your main application");
+            var fullName = GetAssemblyName(assembly) + "." + path;
+            var result = assembly.GetManifestResourceStream(fullName);
+            if (result == null) throw new Exception(ConstructExceptionMessage(path, assembly));
+            return result;
+        }
+
+        private static string ConstructExceptionMessage(string path, Assembly assembly)
+        {
+            const string format = "The resource name '{0}' was not found in assembly '{1}'.";
+            var message = string.Format(format, path, GetAssemblyName(assembly));
+
+            var resourceNames = assembly.GetManifestResourceNames();
+            if (resourceNames.Length == 0) return message + " There are no resources in this assembly.";
+
+            var similarNames = resourceNames.Where(name => path.ToLower().Split(new[] { '.' })
+                .Any(name.ToLower().Contains)).ToArray();
+            if (similarNames.Length > 0)
+            {
+                var nameLength = GetAssemblyName(assembly).Length;
+                similarNames = similarNames.Select(fullName => fullName.Remove(0, nameLength + 1)).ToArray();
+                return message + " Did you mean: " + string.Join(", ", similarNames.ToArray()) + ".";
+            }
+
+            return message;
+        }
+
+        private static string GetAssemblyName(Assembly assembly)
+        {
+            string name = assembly.FullName;
+            var asmName = new AssemblyName(name);
+            return asmName.Name;
         }
     }
 }
